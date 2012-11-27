029f529eb5003c150c066465cf6273f73393f17d
Felix Farquharson <felix.farquharson@googlemail.com>
2012-11-27 07:46:13 +0000

    tried to think about templating things, restructure the templates as a test.

---
+++ view.php
@@ -0,0 +1,22 @@
+<?php
+class View {
+    protected $template_dir = 'templates/';
+    protected $vars = array();
+    public function __construct($template_dir = null) {
+            $this->template_dir = $template_dir;
+    }
+    public function render($template_file) {
+        if (file_exists($this->template_dir.$template_file)) {
+            include $this->template_dir.$template_file;
+        } else {
+            throw new Exception('bugger');
+        }
+    }
+    public function __set($name, $value) {
+        $this->vars[$name] = $value;
+    }
+    public function __get($name) {
+        return $this->vars[$name];
+    }
+}
+?>
