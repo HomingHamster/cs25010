4e3ed468d9ac08897f631702a4b00ef6d22a889c
Felix Farquharson <felix.farquharson@googlemail.com>
2012-11-28 09:15:41 +0000

    cba

--- init.php
+++ init.php
@@ -1,12 +1,13 @@
 <?php
 //Maintain the session.
+session_save_path("/ceri/homes1/f/fef/.php_sessions")
 session_start();
 
 //Setup the template manager
 require_once('view.php');
 $t = new View();
 
-if (isset($_SESSION['name'])){
+if (isset($_SESSION['name'])) {
 	$t->logged_in = True;
 	$t->user_name = $_SESSION['name'];
 } else {
--- shop.php
+++ shop.php
@@ -7,8 +7,6 @@ include_once('init.php');
 if (isset($_POST['name'])){
 	$_SESSION['name'] = $_POST['name'];
 	$t->user_name = $_POST['name'];
-} else if (isset($_SESSION['name'])){
-	$t->user_name = $_SESSION['name'];
 }
 	
 $t->render('shop.phtml');
