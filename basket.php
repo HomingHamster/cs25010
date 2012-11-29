257e0f3d9dae1f2717c59113961b3935381e1781
Felix Farquharson <felix.farquharson@googlemail.com>
2012-11-29 23:05:14 +0000

    added basket file which will eventually deal with the basket case. shop filter on games already added.

---
+++ basket.php
@@ -0,0 +1,10 @@
+<?php
+// basket.php - Author Felix Farquharson (fef)
+
+//set everything up...
+include_once('init.php');
+
+//Render the template without any special
+//attributes because this is a "flat" page.
+$t->render('shop.phtml');
+?>
--- shop.php
+++ shop.php
@@ -10,6 +10,19 @@ if ($t->logged_in == False){
     header("location:login.php");
 }
 
+//so if there is a form posted to this page
+//with the slect thingmejig set, then we need
+//to do somthing about it.
+if (isset($_POST["select"])){
+	$selected_games = $_POST['select'];
+	//If there are actually some games selected
+	//not just an empty form then we want to 
+	//save the selected games in the session.
+	if(!empty($selected_games)) {
+		$_SESSION["basket"] = $selected_games;
+	}
+}
+
 //instantiate a read only connection to the
 //specified database.
 $conn = pg_connect("host=db.dcs.aber.ac.uk port=5432
@@ -37,8 +50,12 @@ if (!$conn){
 	//we loop though all of the rows in the table
 	//as an array and add them into the array we
 	//just created.
+	var_dump($_SESSION["basket"]);
     while ($rowarray = pg_fetch_array($res)){
-        $games_array[] = $rowarray;
+    	foreach($_SESSION["basket"] as $basket_ref){
+    		if ($rowarray['refnumber'] != $basket_ref) {
+    			$games_array[] = $rowarray;
+    		}
     }
 }
 
